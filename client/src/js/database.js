24773314b88dec55e65ce68c90481d17b3562939
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2023-03-06 18:11:16 -0500

    added logic that puts & deletes content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to database'); 
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add(content);
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id) => {
+  console.log('DELETE from database', id);
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
